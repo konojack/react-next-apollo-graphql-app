0f2098172b0b0962b6991f79ee1555c7ff620beb
konojack <konojack@gmail.com>
2022-07-22 15:55:42 +0200

    Creating Header and Nav components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,12 +1,13 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <div>
+      <Header />
       <h2>I am the page component</h2>
       {children}
-    </div> 
+    </div>
   );
 }
 
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,12 +2,14 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
-    <Html lang="en-CA">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
+    return (
+      <Html lang="en-CA">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
