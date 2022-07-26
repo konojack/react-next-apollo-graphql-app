556929a934f3a56f8b612b356a67bea51916fe2d
konojack <konojack@gmail.com>
2022-07-26 13:55:12 +0200

    Fixing Styles Compontent flicker on server render

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getServerSideProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(
+      (App) => (props) => sheet.collectStyles(<App {...props} />)
+    );
+  }
+
   render() {
     return (
       <Html lang="en-CA">
