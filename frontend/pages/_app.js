c19177ec479137c136552d0cb89d9c0228e69851
konojack <konojack@gmail.com>
2022-07-26 13:44:15 +0200

    Basic Styling using styled compontents + NProgress bar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Router } from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
 import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
