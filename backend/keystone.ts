2fdd46bfc7f159ed55aac1ee23a5b120b1e1a9be
konojack <konojack@gmail.com>
2022-07-27 13:22:31 +0200

    Basic Keystone Setup

---
+++ backend/keystone.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long the session will last
+  secret: process.env.SESSION_SECRET, // random string used to sign the session
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: add session values here
+});
